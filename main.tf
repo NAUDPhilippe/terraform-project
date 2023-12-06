e0c7db8f35e5a0871973bfef88e8bd406057b42c
Philippe Naud <philippe.naud@loreal.com>
2023-12-06 15:44:01 +0100

    main.tf creation
    Signed-off-by: Philippe Naud <philippe.naud@loreal.com>

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "google" {
+  project = "itx-sbx-x-moutia"
+  region = "us-centrat1"
+  zone = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
